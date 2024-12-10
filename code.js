db1c825fa1a8479216aa83c10c649c03a21e0fa8
Mgoodno1 <90008902+Mgoodno1@users.noreply.github.com>
2024-12-10 01:48:12 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,31 @@
 function flip(array, n) {
+    let left = 0;
+    let right = n - 1;
+    while (left < right) {
+        [array[left], array[right]] = [array[right], array[left]];
+        left++;
+        right--;
+    }
     return array;
 }
 
-// Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    let n = array.length;
+    
+    for (let currSize = n; currSize > 1; currSize--) {
+        let maxIndex = 0;
+        for (let i = 1; i < currSize; i++) {
+            if (array[i] > array[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        
+        if (maxIndex !== currSize - 1) {
+            if (maxIndex !== 0) {
+                flip(array, maxIndex + 1);
+            }
+            flip(array, currSize);
+        }
+    }
     return array;
 }
